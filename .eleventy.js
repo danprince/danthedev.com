5b7d60426e255ee49042c375c54d410189fc7934
danprince <northwalesdan@gmail.com>
2021-01-13 14:08:58 +0000

    use target=_blank for external links

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,26 @@
 let syntax = require("@11ty/eleventy-plugin-syntaxhighlight");
+let markdown = require("markdown-it");
+let markdownLinkAttrs = require("markdown-it-link-attributes");
 
 module.exports = config => {
   config.addPlugin(syntax);
   config.addPassthroughCopy("styles");
   config.addPassthroughCopy("scripts");
   config.addPassthroughCopy("favicon.ico");
+
+  let md = markdown({
+    html: true,
+    linkify: true,
+    breaks: true,
+  });
+
+  md.use(markdownLinkAttrs, {
+    pattern: /^https?:/,
+    attrs: {
+      target: "_blank",
+      rel: "noopener",
+    }
+  });
+
+  config.setLibrary("md", md);
 }

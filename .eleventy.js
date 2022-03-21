e9985b773b5feb1dcf1c6b4623336c7d7206c23a
Dan Prince <danprince@hey.com>
2022-03-21 14:17:43 +0100

    adds esbuild

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,37 @@
+let path = require("path");
+let esbuild = require("esbuild");
 let syntax = require("@11ty/eleventy-plugin-syntaxhighlight");
 let markdown = require("markdown-it");
 let linkAttrs = require("markdown-it-link-attributes");
 let attrs = require("markdown-it-attrs");
 let anchor = require("markdown-it-anchor");
+
+/**
+ * @param {string[]} entryPoints
+ * @returns {Promise<string>}
+ */
+async function esbuildShortcode(entryPoint) {
+  let directory = path.dirname(this.page.inputPath);
+  entryPoint = path.join(directory, entryPoint);
+
+  let result = await esbuild.build({
+    entryPoints: [entryPoint],
+    entryNames: `[hash]`,
+    minify: true,
+    sourcemap: true,
+    metafile: true,
+    bundle: true,
+    outdir: path.resolve("_site/assets"),
+  });
+
+  let output = Object.keys(result.metafile.outputs).find(key => {
+    let output = result.metafile.outputs[key];
+    return output.entryPoint === entryPoint;
+  });
+
+  output = output.replace(/^_site/, "");
+  return `<script src="${output}"></script>`;
+}
 
 module.exports = config => {
   config.addPlugin(syntax);
@@ -10,7 +39,11 @@ module.exports = config => {
   config.addPassthroughCopy({ "public": "/" })
 
   // These files will be copied through in-place
-  config.setTemplateFormats(["md", "css", "js"]);
+  config.setTemplateFormats(["md", "css"]);
+
+  config.addShortcode("esbuild", esbuildShortcode);
+
+  config.addWatchTarget("**/*.ts");
 
   config.addFilter("filterBySeries", (posts, series) => {
     return posts.filter(post => {

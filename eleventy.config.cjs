17f2830fa105803cd5fbcb1489d0bf187688f5f5
Dan Prince <1266011+danprince@users.noreply.github.com>
2023-01-30 13:26:56 +0300

    runtime/env for islands

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,61 +1,22 @@
-let path = require("path");
-let esbuild = require("esbuild");
 let syntax = require("@11ty/eleventy-plugin-syntaxhighlight");
 let markdown = require("markdown-it");
 let linkAttrs = require("markdown-it-link-attributes");
 let toc = require("markdown-it-table-of-contents");
 let attrs = require("markdown-it-attrs");
 let anchor = require("markdown-it-anchor");
-
-/**
- * @param {string[]} entryPoints
- * @returns {Promise<string>}
- */
-async function esbuildShortcode(entryPoint) {
-  let directory = path.dirname(this.page.inputPath);
-  entryPoint = path.join(directory, entryPoint);
-
-  let result = await esbuild.build({
-    entryPoints: [entryPoint],
-    entryNames: `[hash]`,
-    minify: true,
-    sourcemap: true,
-    metafile: true,
-    bundle: true,
-    outdir: path.resolve("_site/assets"),
-    jsxImportSource: "preact",
-  });
-
-  if (result.errors.length) {
-    for (let err of result.errors) {
-      console.error(err);
-    }
-
-    throw new Error("esbuild bundle error");
-  }
-
-  let tags = Object.keys(result.metafile.outputs).map(key => {
-    let src = key.replace(/^_site/, "");
-    if (/\.js$/.test(src)) return `<script src="${src}"></script>`;
-    if (/\.css$/.test(src)) return `<link rel="stylesheet" href="${src}" />`;
-    return "";
-  });
-
-  return tags.join("\n");
-}
+let islands = require("./11ty/islands.cjs");
 
 /**
  * @param {import("@11ty/eleventy/src/UserConfig")} config
  */
 module.exports = config => {
   config.addPlugin(syntax);
+  config.addPlugin(islands);
 
   config.addPassthroughCopy({ "public": "/" })
 
   // These files will be copied through in-place
   config.setTemplateFormats(["md", "css"]);
-
-  config.addShortcode("esbuild", esbuildShortcode);
 
   config.addWatchTarget("**/*.ts");
   config.addWatchTarget("**/*.tsx");

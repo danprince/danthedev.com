f65bde6ca69ac7d2e4ad9b889ac88d977b9008af
Dan Prince <1266011+danprince@users.noreply.github.com>
2023-02-10 13:42:34 +0300

    removes esbuild

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,3 @@
-let path = require("path");
-let esbuild = require("esbuild");
 let syntax = require("@11ty/eleventy-plugin-syntaxhighlight");
 let markdown = require("markdown-it");
 let footnotes = require("markdown-it-footnote");
@@ -7,43 +5,6 @@ let linkAttrs = require("markdown-it-link-attributes");
 let toc = require("markdown-it-table-of-contents");
 let attrs = require("markdown-it-attrs");
 let anchor = require("markdown-it-anchor");
-
-/**
- * @param {string[]} entryPoints
- * @returns {Promise<string>}
- */
-async function esbuildShortcode(entryPoint) {
-  let directory = path.dirname(this.page.inputPath);
-  entryPoint = path.join(directory, entryPoint);
-
-  let result = await esbuild.build({
-    entryPoints: [entryPoint],
-    entryNames: `[hash]`,
-    minify: true,
-    sourcemap: true,
-    metafile: true,
-    bundle: true,
-    outdir: path.resolve("_site/assets"),
-    jsxImportSource: "preact",
-  });
-
-  if (result.errors.length) {
-    for (let err of result.errors) {
-      console.error(err);
-    }
-
-    throw new Error("esbuild bundle error");
-  }
-
-  let tags = Object.keys(result.metafile.outputs).map(key => {
-    let src = key.replace(/^_site/, "");
-    if (/\.js$/.test(src)) return `<script src="${src}"></script>`;
-    if (/\.css$/.test(src)) return `<link rel="stylesheet" href="${src}" />`;
-    return "";
-  });
-
-  return tags.join("\n");
-}
 
 /**
  * @param {import("@11ty/eleventy/src/UserConfig")} config
@@ -55,11 +16,6 @@ module.exports = config => {
 
   // These files will be copied through in-place
   config.setTemplateFormats(["md", "css"]);
-
-  config.addShortcode("esbuild", esbuildShortcode);
-
-  config.addWatchTarget("**/*.ts");
-  config.addWatchTarget("**/*.tsx");
 
   let md = markdown({
     html: true,
